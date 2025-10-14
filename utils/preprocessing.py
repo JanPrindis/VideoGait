ac62cf821fb7a2925ae73e2c1d3762dc833a687b
Jan Prindis <jan.prindis3@gmail.com>
2025-10-14 17:12:21 +0200

    Added relative prominence option for local minima/maxima search

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.signal import butter, filtfilt, find_peaks
 from scipy.interpolate import CubicSpline
 
+
 def cubic_interpolate_nan(y):
     y = np.array([np.nan if v is None else v for v in y], dtype=float)
     mask = ~np.isnan(y)
@@ -21,13 +22,41 @@ def cubic_interpolate_nan(y):
 
     return np.array(y_interp)
 
+
 def butterworth_filter(data, cutoff=5, fs=60.0, order=5):
     normal_cutoff = cutoff / (fs / 2)
     b, a = butter(order, normal_cutoff, btype='low')
     y = filtfilt(b, a, data)
     return np.array(y)
 
-def find_minima_maxima(data, distance=20, prominence=5):
+
+def find_minima_maxima(data, distance=20, prominence=None, rel_prominence=0.3):
+    """
+    Detect local minima and maxima in a 1D signal.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        1D array representing the input signal.
+    distance : int, optional
+        Minimum number of samples between consecutive peaks (default: 20).
+        Helps to avoid detecting multiple peaks too close to each other.
+    prominence : float or None, optional
+        Minimum prominence of peaks to be considered valid. If None, it is computed
+        automatically as percentage (`rel_prominence`) of the total peak-to-peak amplitude of the signal.
+    rel_prominence : float, optional (default: 0.3)
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray]
+        Two arrays of the same length as `data`:
+        - First array contains detected **minima** values at their positions, and `None` elsewhere.
+        - Second array contains detected **maxima** values at their positions, and `None` elsewhere.
+    """
+
+    if prominence is None:
+        prominence = np.ptp(data) * rel_prominence
+
     minima, _ = find_peaks(-data, distance=distance, prominence=prominence)
     maxima, _ = find_peaks(data, distance=distance, prominence=prominence)
     minima_out = [None] * len(data)
