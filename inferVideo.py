3babe02da43f9819ac90255fc9d3a16f83604683
Jan Prindis <jan.prindis3@gmail.com>
2025-10-14 21:13:02 +0200

    Added progress bar

--- inferVideo.py
+++ inferVideo.py
@@ -3,6 +3,7 @@ from functools import partial
 
 import cv2
 import numpy as np
+from tqdm import tqdm
 
 from rtmlib import BodyWithFeet, PoseTracker, RTMDet, RTMPose, Custom
 from utils.jsonSerializer import KeypointSerializer
@@ -33,9 +34,14 @@ tracker = PoseTracker(
     tracking=True,
 )
 
-cap = cv2.VideoCapture("videos/logitech-1920-60-8.avi")  # Video file path
-file_name = "test"
+# cap = cv2.VideoCapture("videos/logitech-1920-60-8.avi")  # Video file path
+cap = cv2.VideoCapture("videos/interpolated.mp4")  # Video file path
+tot_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+file_name = "interpolated"
 serializer = KeypointSerializer("./results/", f"{file_name}.json")
+
+pbar = tqdm(total=tot_frame)
 
 frame_idx = 0
 while cap.isOpened():
@@ -48,6 +54,7 @@ while cap.isOpened():
     keypoints, scores = tracker(frame)
 
     if len(scores) == 0:
+        pbar.update(1)
         continue
 
     total_scores = [sum(s) for s in scores]
@@ -55,9 +62,6 @@ while cap.isOpened():
 
     filtered_keypoints = keypoints[np.newaxis, best_idx]
     filtered_scores = scores[np.newaxis, best_idx]
-
-    det_time = time.time() - s
-    print('det: ', det_time)
 
     frame_kpts = np.hstack([filtered_keypoints[0], filtered_scores[0][:, None]]).flatten().tolist()
 
@@ -67,6 +71,7 @@ while cap.isOpened():
     )
 
     frame_idx += 1
+    pbar.update(1)
 
 serializer.save()
 cap.release()
