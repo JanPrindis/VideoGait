8811c8ec8b09f4469b7304cb9965c5da1d765c35
Jan Prindis <jan.prindis3@gmail.com>
2025-10-21 22:47:37 +0200

    Changed to batch processing

--- annotate.py
+++ annotate.py
@@ -1,17 +1,42 @@
+from venv import create
+
 import cv2
+import glob
 
 from Skeletons.halpe_skeleton import HALPE_SKELETON
 from utils.visualizer import Visualizer
+from utils.data import create_folder_if_not_exists
 
-file_name = "test"
+video_folder = "silhouette"
+result_folder = "sil"
 
-vis = Visualizer(skeleton_definition=HALPE_SKELETON)
-vis.visualize(
-    original_video="./videos/logitech-1920-60-8.avi",
-    visualizer_output_path="./results",
-    visualizer_output_file=f"{file_name}.mp4",
-    detector_output_path="./results",
-    detector_output_file=f"{file_name}.json",
-    confidence_threshold=0.6
-)
+create_folder_if_not_exists(f"./videoResults/{result_folder}")
+
+files = glob.glob(f"./results/{result_folder}/*.json")
+files.sort()
+
+for file_path in files:
+    file_name = file_path.split("\\")[-1].split(".")[0]
+
+    vis = Visualizer(skeleton_definition=HALPE_SKELETON)
+    vis.visualize(
+        original_video=f"D:/Semestralka/Health_Gait/generated_videos/{video_folder}/{file_name}.mp4",
+        visualizer_output_path=f"./videoResults/{result_folder}",
+        visualizer_output_file=f"{file_name}.mp4",
+        detector_output_path=f"./results/{result_folder}",
+        detector_output_file=f"{file_name}.json",
+        confidence_threshold=0.6
+    )
+
+#file_name = "patient_00"
+
+# vis = Visualizer(skeleton_definition=HALPE_SKELETON)
+# vis.visualize(
+#     original_video="./videos/patient_00_i.mp4",
+#     visualizer_output_path="./videoResults",
+#     visualizer_output_file=f"{file_name}.mp4",
+#     detector_output_path="./results",
+#     detector_output_file=f"{file_name}.json",
+#     confidence_threshold=0.6
+# )
 
--- inferVideo.py
+++ inferVideo.py
@@ -1,4 +1,6 @@
+import glob
 from functools import partial
+from venv import create
 
 import cv2
 import numpy as np
@@ -6,6 +8,7 @@ from tqdm import tqdm
 
 from rtmlib import PoseTracker, Custom
 from utils.jsonSerializer import KeypointSerializer
+from utils.data import create_folder_if_not_exists
 
 device = 'cuda'
 backend = 'onnxruntime'  # opencv, onnxruntime, openvino
@@ -35,62 +38,75 @@ tracker = PoseTracker(
 
 MIN_BBOX_SIZE = 250
 
-# cap = cv2.VideoCapture("videos/logitech-1920-60-8.avi")  # Video file path
-cap = cv2.VideoCapture("videos/test_interpolated.mp4")  # Video file path
-tot_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+in_video_root_path = "D:/Semestralka/Health_Gait/generated_videos/silhouette"
+videos = glob.glob(f"{in_video_root_path}/*.mp4")[0:10]
+out_prefix = "sil"
+pbar_videos = tqdm(videos, desc="Processing all videos")
 
-file_name = "interpolated"
-serializer = KeypointSerializer("./results/", f"{file_name}.json")
+create_folder_if_not_exists(f"./results/{out_prefix}")
 
-pbar = tqdm(total=tot_frame)
+for i, video in enumerate(videos):
+    # cap = cv2.VideoCapture("videos/logitech-1920-60-8.avi")  # Video file path
+    # cap = cv2.VideoCapture("videos/test_interpolated.mp4")  # Video file path
+    # cap = cv2.VideoCapture("videos/patient_00_i.mp4")  # Video file path
 
-frame_idx = 0
-while cap.isOpened():
-    success, frame = cap.read()
+    cap = cv2.VideoCapture(video)  # Video file path
+    tot_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    if not success:
-        break
+    # file_name = "patient_00"
+    serializer = KeypointSerializer(f"./results/{out_prefix}", f"patient_{i:03d}.json")
 
-    keypoints, scores = tracker(frame)
+    pbar = tqdm(total=tot_frame, leave=False, desc=f"Current video progress")
 
-    if len(scores) == 0:
+    frame_idx = 0
+    while cap.isOpened():
+        success, frame = cap.read()
+
+        if not success:
+            break
+
+        keypoints, scores = tracker(frame)
+
+        if len(scores) == 0:
+            frame_idx += 1
+            pbar.update(1)
+            continue
+
+        valid_detections = []
+
+        # Calculate bbox size and filter oun small detections
+        for i, (kpts, sc) in enumerate(zip(keypoints, scores)):
+            x_min, y_min = np.min(kpts[:, 0]), np.min(kpts[:, 1])
+            x_max, y_max = np.max(kpts[:, 0]), np.max(kpts[:, 1])
+            width, height = x_max - x_min, y_max - y_min
+
+            if height >= MIN_BBOX_SIZE:
+                total_score = float(np.sum(sc))
+                valid_detections.append((i, total_score, (x_min, y_min, x_max, y_max)))
+
+        if not valid_detections:
+            frame_idx += 1
+            pbar.update(1)
+            continue
+
+        # Get the detection with the highest score
+        best_idx, best_score, best_bbox = max(valid_detections, key=lambda x: x[1])
+
+        filtered_keypoints = keypoints[np.newaxis, best_idx]
+        filtered_scores = scores[np.newaxis, best_idx]
+
+        frame_kpts = np.hstack([filtered_keypoints[0], filtered_scores[0][:, None]]).flatten().tolist()
+
+        serializer.add_frame(
+            frame_number=frame_idx,
+            keypoints=frame_kpts
+        )
+
         frame_idx += 1
         pbar.update(1)
-        continue
 
-    valid_detections = []
+    serializer.save()
+    cap.release()
+    pbar_videos.update(1)
 
-    # Calculate bbox size and filter oun small detections
-    for i, (kpts, sc) in enumerate(zip(keypoints, scores)):
-        x_min, y_min = np.min(kpts[:, 0]), np.min(kpts[:, 1])
-        x_max, y_max = np.max(kpts[:, 0]), np.max(kpts[:, 1])
-        width, height = x_max - x_min, y_max - y_min
-
-        if height >= MIN_BBOX_SIZE:
-            total_score = float(np.sum(sc))
-            valid_detections.append((i, total_score, (x_min, y_min, x_max, y_max)))
-
-    if not valid_detections:
-        frame_idx += 1
-        pbar.update(1)
-        continue
-
-    # Get the detection with the highest score
-    best_idx, best_score, best_bbox = max(valid_detections, key=lambda x: x[1])
-
-    filtered_keypoints = keypoints[np.newaxis, best_idx]
-    filtered_scores = scores[np.newaxis, best_idx]
-
-    frame_kpts = np.hstack([filtered_keypoints[0], filtered_scores[0][:, None]]).flatten().tolist()
-
-    serializer.add_frame(
-        frame_number=frame_idx,
-        keypoints=frame_kpts
-    )
-
-    frame_idx += 1
-    pbar.update(1)
-
-serializer.save()
-cap.release()
-cv2.destroyAllWindows()
+pbar_videos.close()
