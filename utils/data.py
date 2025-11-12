6c61fc648b52f07732ab4a27d228fea1ca2e469d
Jan Prindis <jan.prindis3@gmail.com>
2025-11-12 21:27:26 +0100

    Refactoring

--- utils/data.py
+++ utils/data.py
@@ -15,10 +15,6 @@ def trim(data, first_valid, last_valid):
     return data[first_valid:last_valid + 1]
 
 
-def extract_keypoints(keypoint_list, index):
-    return keypoint_list[index * 3], keypoint_list[index * 3 + 1], keypoint_list[index * 3 + 2]
-
-
 def get_valid_range(hip: np.array, frame_rate: float, exclude_percent: float = 0.1):
     # Preprocessing
     hip = cubic_interpolate_nan(hip)
