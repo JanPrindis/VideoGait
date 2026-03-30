74e8ff5778e45d1f6e51b7ef32de7440b52dfa9e
Jan Prindis <jan.prindis3@gmail.com>
2026-03-30 15:56:58 +0200

    Update UI

--- tools/annotator.py
+++ tools/annotator.py
@@ -9,7 +9,7 @@ from utils.json_serializer import AnnotationSerializer
 
 
 class GaitAnnotator:
-    def __init__(self, save_root="./annotations"):
+    def __init__(self, save_root="../annotations"):
         self.video_path = self.select_video_file()
         if not self.video_path:
             print("Error, no video selected")
@@ -29,6 +29,7 @@ class GaitAnnotator:
         self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
         self.current_frame = 0
 
+        self.last_action_message = "Started."
     @staticmethod
     def select_video_file():
         root = tk.Tk()
@@ -42,15 +43,22 @@ class GaitAnnotator:
     def annotate(self):
         cv2.namedWindow("Gait Annotator", cv2.WINDOW_NORMAL)
 
-        while True:
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_frame)
-            ret, frame = self.cap.read()
-            if not ret:
-                break
+        last_frame_idx = -1
+        current_image = None
 
-            frame_disp = frame.copy()
-            cv2.putText(frame_disp, f"Frame: {self.current_frame}/{self.frame_count}",
-                        (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        while True:
+            if self.current_frame != last_frame_idx:
+                if self.current_frame != last_frame_idx + 1:
+                    self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_frame)
+                
+                ret, frame = self.cap.read()
+                if not ret:
+                    break
+                current_image = frame
+                last_frame_idx = self.current_frame
+
+            frame_disp = current_image.copy()
+            self._draw_hud(frame_disp)
 
             cv2.imshow("Gait Annotator", frame_disp)
             key = cv2.waitKeyEx(0)
@@ -72,34 +80,83 @@ class GaitAnnotator:
 
             elif key == ord('a'):
                 self.left_events.append(GaitEvent(self.current_frame, GaitEventType.HEEL_STRIKE))
-                print(f"[L] Heel strike @ {self.current_frame}")
+                msg = f"[L] Heel strike @ {self.current_frame}"
+                print(msg)
+                self.last_action_message = msg
 
             elif key == ord('q'):
                 self.left_events.append(GaitEvent(self.current_frame, GaitEventType.TOE_OFF))
-                print(f"[L] Toe off @ {self.current_frame}")
+                msg = f"[L] Toe off @ {self.current_frame}"
+                print(msg)
+                self.last_action_message = msg
 
             elif key == ord('s'):
                 self.right_events.append(GaitEvent(self.current_frame, GaitEventType.HEEL_STRIKE))
-                print(f"[R] Heel strike @ {self.current_frame}")
+                msg = f"[R] Heel strike @ {self.current_frame}"
+                print(msg)
+                self.last_action_message = msg
 
             elif key == ord('w'):
                 self.right_events.append(GaitEvent(self.current_frame, GaitEventType.TOE_OFF))
-                print(f"[R] Toe off @ {self.current_frame}")
+                msg = f"[R] Toe off @ {self.current_frame}"
+                print(msg)
+                self.last_action_message = msg
 
             elif key == ord('z'):
                 if len(self.left_events) == 0:
                     continue
                 e = self.left_events.pop()
-                print(f"[L] Removed last event: {e}")
+                msg = f"[L] Removed: {e.event_type.name} @ {e.frame}"
+                print(msg)
+                self.last_action_message = msg
 
             elif key == ord('x'):
                 if len(self.right_events) == 0:
                     continue
                 e = self.right_events.pop()
-                print(f"[R] Removed last event: {e}")
+                msg = f"[R] Removed: {e.event_type.name} @ {e.frame}"
+                print(msg)
+                self.last_action_message = msg
 
         self.cap.release()
         cv2.destroyAllWindows()
+
+    def _draw_hud(self, frame):
+        overlay = frame.copy()
+        cv2.rectangle(overlay, (10, 10), (450, 160), (0, 0, 0), -1)
+        cv2.addWeighted(overlay, 0.6, frame, 0.4, 0, frame)
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 0.7
+        thickness = 2
+        y_pos = 40
+        x_pos = 20
+
+        def draw_text(text, y, color, scale=font_scale, th=thickness):
+            cv2.putText(frame, text, (x_pos, y), font, scale, (0, 0, 0), th + 2, cv2.LINE_AA)
+            cv2.putText(frame, text, (x_pos, y), font, scale, color, th, cv2.LINE_AA)
+
+        # Frame counter
+        frame_text = f"Frame: {self.current_frame} / {self.frame_count}"
+        draw_text(frame_text, y_pos, (50, 255, 50))
+        y_pos += 35
+
+        # Event counters
+        left_hs = sum(1 for e in self.left_events if e.event_type == GaitEventType.HEEL_STRIKE)
+        left_to = sum(1 for e in self.left_events if e.event_type == GaitEventType.TOE_OFF)
+        right_hs = sum(1 for e in self.right_events if e.event_type == GaitEventType.HEEL_STRIKE)
+        right_to = sum(1 for e in self.right_events if e.event_type == GaitEventType.TOE_OFF)
+
+        left_text = f"Left : HS={left_hs}, TO={left_to}"
+        right_text = f"Right: HS={right_hs}, TO={right_to}"
+        draw_text(left_text, y_pos, (255, 200, 50))
+        y_pos += 30
+        draw_text(right_text, y_pos, (50, 150, 255))
+        y_pos += 40
+
+        # Last action
+        action_text = f"Last: {self.last_action_message}"
+        draw_text(action_text, y_pos, (230, 230, 230), scale=0.6, th=1)
 
     def save_annotations(self):
         base_name = os.path.splitext(os.path.basename(self.video_path))[0]
